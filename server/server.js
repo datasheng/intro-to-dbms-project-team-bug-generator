f285fb5df791059ae6adb3c1ee0c89ebcc692c77
Kareem Virani <57181972+kareemv@users.noreply.github.com>
2024-07-22 22:22:55 -0400

    Add cors middleware to server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const db = require("./db/config/db.config");
 const createTableQueries = require("./db/models/createTables");
 const authRoutes = require("./routes/auth");
@@ -8,6 +9,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/auth", authRoutes);
 
 const createTables = async () => {
